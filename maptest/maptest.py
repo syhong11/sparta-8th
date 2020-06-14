7953c87fd722b71d8949c78c53bf4e6fa0486126
syhong11 <64946909+syhong11@users.noreply.github.com>
2020-06-14 13:06:23 +0900

    add

---
+++ maptest/maptest.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, jsonify
+from pymongo import MongoClient
+
+client = MongoClient('localhost', port=27017)
+db = client.schoolzonedb
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('maptest.html')
+
+# @app.route('/api/list')
+# def star_retreive():
+#     stars = list(
+#         db.school.find({}, {'_id': False, 'Latitude': 1, 'Longitude': 1)
+#     return jsonify({
+#         'result': 'success',
+#         'school': school
+#     })
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
