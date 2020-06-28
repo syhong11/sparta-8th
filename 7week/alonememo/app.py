cd13972a4549d5734da31ecdc79a5b43f3046cf5
syhong11 <64946909+syhong11@users.noreply.github.com>
2020-06-28 12:56:04 +0900

    추가

--- 7week/alonememo/app.py
+++ 7week/alonememo/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 import requests
 from bs4 import BeautifulSoup
 
-from pymongo import MongoClient           # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
+from pymongo import MongoClient           # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)\
 client = MongoClient('mongodb://test:test@localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
 db = client.dbsparta                      # 'dbsparta'라는 이름의 db를 만듭니다.
 
---
+++ project/schoolzone.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request, jsonify
+
+import requests
+from bs4 import BeautifulSoup
+from pymongo import MongoClient
+
+client = MongoClient('localhost', 27017)
+# client = MongoClient('mongodb://test:test@localhost', 27017)   
+db = client.schoolzonedb
+# db = client.schoolzoneDB
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('school.html')
+
+@app.route('/api/list')
+def points_retreive():
+    region_receive = request.args.get('region_give')
+
+    points = list(db.school.find({'Agency': { '$regex' : region_receive, '$options' : 'i' }}, {'_id': False, 'Latitude': True, 'Longitude': True}))
+
+    return jsonify({
+        'result': 'success',
+        'school': points
+    })
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
